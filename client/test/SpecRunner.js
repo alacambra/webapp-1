405c1632b7e0a56e660d3ba6ee19124bfcc49d0e
Thomas Stern <thomas.stern@ion2s.com>
2013-12-17 12:40:24 +0100

    - added tests for task app

--- client/test/SpecRunner.js
+++ client/test/SpecRunner.js
@@ -64,6 +64,7 @@ require([ 'jasmine-html', 'underscore', 'jquery' ], function (jasmine, _, $) {
 
     specs.push('spec/app.spec');
     specs.push('spec/app/entities/task.spec');
+    specs.push('spec/app/tasks/tasks_app.spec');
     specs.push('spec/app/tasks/task_helper.spec');
     specs.push('spec/app/tasks/edit/edit_controller.spec.js');
     specs.push('spec/app/tasks/edit/edit_view.spec.js');
---
+++ client/test/spec/app/tasks/tasks_app.spec.js
@@ -0,0 +1,52 @@
+define([ 'app', 'app/tasks/tasks_app' ], function (App, TasksApp) {
+
+    describe('Task :: App', function () {
+
+        it('Navigate to tasks list.', function () {
+            spyOn(App, 'navigate');
+
+            App.trigger('tasks:list');
+
+            expect(App.navigate).toHaveBeenCalledWith('tasks');
+        });
+
+        it('Navigate to new task.', function () {
+            spyOn(App, 'navigate');
+
+            App.trigger('task:new');
+
+            expect(App.navigate).toHaveBeenCalledWith('tasks/new');
+        });
+
+        it('Navigate to show task.', function () {
+            var id = 1;
+
+            spyOn(App, 'navigate');
+
+            App.trigger('task:show', id);
+
+            expect(App.navigate).toHaveBeenCalledWith('tasks/' + id);
+        });
+
+        it('Navigate to edit task.', function () {
+            var id = 1;
+
+            spyOn(App, 'navigate');
+
+            App.trigger('task:edit', id);
+
+            expect(App.navigate).toHaveBeenCalledWith('tasks/' + id + '/edit');
+        });
+
+        it('Confirm to delete task.', function () {
+            var id = 1,
+                redirect = 'redirect';
+
+            spyOn(window, 'confirm');
+
+            App.trigger('task:delete', id, redirect);
+
+            expect(window.confirm).toHaveBeenCalledWith(jasmine.any(String));
+        });
+    });
+});
