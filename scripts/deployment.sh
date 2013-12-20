9099e7f8b240b22ea03b44240cf98caf4a660e42
Albert Lacambra <albert.lacambra@ion2s.com>
2013-12-20 13:25:34 +0100

    - script changes

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
-cd  /home/alacambra/webapp
-git pull origin deployment-testing
-git checkout deployment-testing 
-cp -Rf /home/alacambra/webapp/server/ /home/alacambra/deployment/
-cp -Rf /home/alacambra/webapp/client/* /home/alacambra/deployment/server/webapplication/src/main/webapp/
-cd /home/alacambra/deployment/server/webapplication/
+
+client_tag=$1
+server_tag=$2
+deployment_branch=$3
+base_path=$4
+deployment_folder=$base_path/deployment
+
+if [ $# != 4 ]; then
+    echo "exactly 4 arguments needed"
+    exit 1
+fi
+
+echo $client_tag
+echo $server_tag
+echo $deployment_branch
+echo $base_path
+
+cd  $base_path/webapp
+
+git pull origin $deployment_branch
+git checkout $deployment_branch
+
+cp -Rf $base_path/webapp/server/ $deployment_folder
+cp -Rf $base_path/webapp/client/* $deployment_folder/server/webapplication/src/main/webapp/
+
+cd $deployment_folder/server/webapplication/
 mvn clean package -P dep
 mvn glassfish:redeploy -P dep
