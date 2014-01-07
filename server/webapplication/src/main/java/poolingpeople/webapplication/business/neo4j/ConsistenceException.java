5499cb173207411bda816b6c6cd71b022d106ea0
Albert Lacambra <albert.lacambra@ion2s.com>
2014-01-07 18:24:53 +0100

    some changes

---
+++ server/webapplication/src/main/java/poolingpeople/webapplication/business/neo4j/ConsistenceException.java
@@ -0,0 +1,22 @@
+package poolingpeople.webapplication.business.neo4j;
+
+import javax.inject.Inject;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import poolingpeople.webapplication.business.boundary.RootApplicationException;
+import poolingpeople.webapplication.business.utils.configuration.boundary.Configurable;
+
+public class ConsistenceException extends RootApplicationException {
+
+    private static final long serialVersionUID = -2956832836211447594L;
+
+    public ConsistenceException(String text) {
+        super(text);
+    }
+
+    @Override
+    public Response getSpecificWebResponse() {
+        return Response.status(Status.INTERNAL_SERVER_ERROR).entity(getMessage()).build();
+    }
+
+}
