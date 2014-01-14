de3d084cd6dd45e7f61a2923d6b790c78c49bf5e
Sascha Hanssen <sascha.hanssen@ion2s.com>
2014-01-14 14:57:58 +0100

    - fix: i18n dependency was not defined correct (broke dist build)

--- client/js/app.js
+++ client/js/app.js
@@ -1,4 +1,4 @@
-define(['marionette', 'config'], function(Marionette, CONFIG){
+define(['marionette', 'config', 'i18n'], function(Marionette, CONFIG){
     var App = new Marionette.Application();
 
 
