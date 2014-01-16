87c48e16438a272030111bcca1778fec1aaa860d
Albert Lacambra <albert.lacambra@ion2s.com>
2014-01-16 15:45:09 +0100

    - dep script

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -11,18 +11,16 @@ fi
 
 echo $deployment_branch
 echo $base_path
+echo $deployment_folder
 
 cd $deployment_folder/webapp
 git checkout $deployment_branch
 git pull
-cd $deployment_folder/webapp/client/
-cp js/config.example.js js/config.js
+cd $deployment_folder/webapp/client/js
+cp ./config.example.js ./config.js
+cat ./config.js
 grunt build
-rm js/config.js
-cp -Rf $base_path/webapp/server/ $deployment_folder
-cp -Rf $base_path/webapp/client/dist/* $deployment_folder/server/webapplication/src/main/webapp/
-
-cd $deployment_folder/server/webapplication/
-mvn clean package -P dep
-mvn glassfish:redeploy -P dep
-
+cp -Rf $deployment_folder/webapp/client/dist/* $deployment_folder/webapp/server/webapplication/src/main/webapp/
+cd $deployment_folder/webapp/server/webapplication/
+mvn clean package glassfish:redeploy -P dep
+git reset --hard HEAD
