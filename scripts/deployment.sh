d35b28d57cd652cc51db9c4124c91a04f3654fc8
Albert Lacambra <albert.lacambra@ion2s.com>
2014-01-16 14:09:30 +0100

    script changes

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,31 +1,28 @@
 #!/bin/bash
 
-client_tag=$1
-server_tag=$2
-deployment_branch=$3
-base_path=$4
-deployment_folder=$base_path/deployment
+deployment_branch="develop"
+base_path=$1
+deployment_folder=$base_path/deployments
 
-if [ $# != 4 ]; then
+if [ $# != 1 ]; then
     echo "exactly 4 arguments needed"
     exit 1
 fi
 
-echo $client_tag
-echo $server_tag
 echo $deployment_branch
 echo $base_path
 
-cd  $base_path/webapp
-
-git branch $deployment_branch
+cd $deployment_folder/webapp
 git checkout $deployment_branch
-git merge $client_tag
-git merge $server_tag
-
+git pull
+cd $deployment_folder/webapp/client/
+cp js/config.example.js js/config.js
+grunt build
+rm js/config.js
 cp -Rf $base_path/webapp/server/ $deployment_folder
-cp -Rf $base_path/webapp/client/* $deployment_folder/server/webapplication/src/main/webapp/
+cp -Rf $base_path/webapp/client/dist/* $deployment_folder/server/webapplication/src/main/webapp/
 
 cd $deployment_folder/server/webapplication/
 mvn clean package -P dep
 mvn glassfish:redeploy -P dep
+
