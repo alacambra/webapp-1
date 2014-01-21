131bc7de42656f4bb91f4f91247986aa754c8e4b
Sascha Hanssen <sascha.hanssen@ion2s.com>
2014-01-21 18:26:01 +0100

    - fix: wrong endDate validation message for projects

--- client/js/app/entities/project.js
+++ client/js/app/entities/project.js
@@ -22,7 +22,7 @@ define(['app', 'config', 'app/validation_helper', 'backbone_faux_server'], funct
 
                 if (attrs.startDate != 0 && attrs.endDate != 0) {
                     errors = validation_helper.validates_inclusion_of('endDate', attrs.startDate, attrs.endDate, attrs, errors, {
-                        message : I18n.t('errors.validation.date_later_than', { attr: I18n.t('project.label.start_date') })
+                        message : I18n.t('errors.validation.date_earlier_than', { attr: I18n.t('project.label.start_date') })
                     });
                 }
 
