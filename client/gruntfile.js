d18f700e6911d921e4bec453c77ae9f5a0fa0574
Thomas Stern <thomas.stern@ion2s.com>
2014-01-22 16:30:03 +0100

    - removed comment in build:test grunt task

--- client/gruntfile.js
+++ client/gruntfile.js
@@ -191,7 +191,7 @@ module.exports = function (grunt) {
     ]);
 
     grunt.registerTask('build:test', [
-//        'build',
+        'build',
         'connect:test',
         'jasmine:build'
     ]);
