5d6784598ffed461ac9fd5b026de44a7c12e3029
Sascha Hanssen <sascha.hanssen@ion2s.com>
2014-01-24 09:33:28 +0100

    - converted fixtures to json conform syntax

--- client/test/fixtures/projects.js
+++ client/test/fixtures/projects.js
@@ -1,37 +1,37 @@
 define({
-    0: {
-        id: 0,
-        title: 'Project0',
-        description: "Sed diam nonumy *eirmod tempor* invidunt ut -labore et dolore- magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\n\nStet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        startDate: 1387206224,
-        endDate: 1388206224,
-        status: 2,
+    "0": {
+        "id": 0,
+        "title": "Project0",
+        "description": "Sed diam nonumy *eirmod tempor* invidunt ut -labore et dolore- magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum.\n\nStet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
+        "status": 2,
 
-        taskCount: 2
+        "taskCount": 2
     },
-    1: {
-        id: 1,
-        title: 'Project1',
-        startDate: 1387206224,
-        endDate: 1388206224,
+    "1": {
+        "id": 1,
+        "title": "Project1",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
 
-        taskCount: 1
+        "taskCount": 1
     },
-    2: {
-        id: 2,
-        title: 'Project2',
-        description: 'Lorem ipsum',
-        startDate: 1387206224
+    "2": {
+        "id": 2,
+        "title": "Project2",
+        "description": "Lorem ipsum",
+        "startDate": 1387206224
     },
-    3: {
-        id: 3,
-        title: 'Project3',
-        endDate: 1388206224,
-        status: 1
+    "3": {
+        "id": 3,
+        "title": "Project3",
+        "endDate": 1388206224,
+        "status": 1
     },
-    4: {
-        id: 4,
-        title: 'Project4',
-        status: 2
+    "4": {
+        "id": 4,
+        "title": "Project4",
+        "status": 2
     }
 });
--- client/test/fixtures/tasks.js
+++ client/test/fixtures/tasks.js
@@ -1,86 +1,86 @@
 define({
-    0: {
-        id: 0,
-        title: 'Task0',
-        description: "Lorem ipsum dolor sit amet, _consetetur sadipscing_ elitr.\n\nSed diam *nonumy eirmod tempor* invidunt ut\n\n# labore et dolore\n# magna aliquyam erat\n# sed diam voluptua\n\nAt vero eos et\n\n* accusam et justo\n* duo dolores et ea\n\nRebum stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
-        startDate: 1387206224,
-        endDate: 1388206224,
-        progress: 0.8,
+    "0": {
+        "id": 0,
+        "title": "Task0",
+        "description": "Lorem ipsum dolor sit amet, _consetetur sadipscing_ elitr.\n\nSed diam *nonumy eirmod tempor* invidunt ut\n\n# labore et dolore\n# magna aliquyam erat\n# sed diam voluptua\n\nAt vero eos et\n\n* accusam et justo\n* duo dolores et ea\n\nRebum stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
+        "progress": 0.8,
 
-        effort: 45,
-        project: {
-            id: 0,
-            title: 'Project0'
+        "effort": 45,
+        "project": {
+            "id": 0,
+            "title": "Project0"
         },
-        subtaskCount: 2
+        "subtaskCount": 2
     },
-    1: {
-        id: 1,
-        title: 'Task1',
-        description: 'Lorem ipsum',
-        startDate: 1387206224,
-        endDate: 1388206224,
+    "1": {
+        "id": 1,
+        "title": "Task1",
+        "description": "Lorem ipsum",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
 
-        effort: 315,
-        project: {
-            id: 0,
-            title: 'Project0'
+        "effort": 315,
+        "project": {
+            "id": 0,
+            "title": "Project0"
         }
     },
-    2: {
-        id: 2,
-        title: 'Task2',
-        startDate: 1387206224,
-        endDate: 1388206224,
-        progress: 1,
+    "2": {
+        "id": 2,
+        "title": "Task2",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
+        "progress": 1,
 
-        effort: 120,
-        project: {
-            id: 1,
-            title: 'Project1'
+        "effort": 120,
+        "project": {
+            "id": 1,
+            "title": "Project1"
         }
     },
-    3: {
-        id: 3,
-        title: 'Task3',
-        description: 'Lirum larum',
-        startDate: 1387206224,
-        endDate: 1388206224,
-        progress: 0.2,
+    "3": {
+        "id": 3,
+        "title": "Task3",
+        "description": "Lirum larum",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
+        "progress": 0.2,
 
-        parent_task: {
-            id: 0,
-            title: 'Task0'
+        "parent_task": {
+            "id": 0,
+            "title": "Task0"
         },
-        subtaskCount: 1
+        "subtaskCount": 1
     },
-    4: {
-        id: 4,
-        title: 'Task4',
-        startDate: 1387206224,
-        endDate: 1388206224,
-        progress: 0.5,
+    "4": {
+        "id": 4,
+        "title": "Task4",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
+        "progress": 0.5,
 
-        parent_task: {
-            id: 0,
-            title: 'Task0'
+        "parent_task": {
+            "id": 0,
+            "title": "Task0"
         }
     },
-    5: {
-        id: 5,
-        title: 'Task5',
-        startDate: 1387206224,
-        endDate: 1388206224,
-        progress: 0.8,
+    "5": {
+        "id": 5,
+        "title": "Task5",
+        "startDate": 1387206224,
+        "endDate": 1388206224,
+        "progress": 0.8,
 
-        parent_task: {
-            id: 3,
-            title: 'Task3'
+        "parent_task": {
+            "id": 3,
+            "title": "Task3"
         }
     },
-    6: {
-        id: 6,
-        title: 'Task6',
-        startDate: 1387206224
+    "6": {
+        "id": 6,
+        "title": "Task6",
+        "startDate": 1387206224
     }
 });
