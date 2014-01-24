2a79744327d36bdc89837596c1e1ab51888e5ae8
Thomas Stern <thomas.stern@ion2s.com>
2014-01-24 16:39:59 +0100

    - extended test for effort entity

--- client/test/spec/app/entities/effort.spec.js
+++ client/test/spec/app/entities/effort.spec.js
@@ -1,11 +1,31 @@
-define(['app/entities/effort'], function(Entities) {
+define(['app',
+        'app/entities/effort'],
+function(App, Entities) {
     return describe('Effort :: Entities', function() {
+
         var effort = null,
-            efforts = null;
+            efforts = null,
+            temp_effort_fetch = null,
+            temp_efforts_fetch = null;
 
         beforeEach(function() {
-            effort = new Entities.Effort({}, { task_id: 10 });
+            effort = new Entities.Effort({ id: 5 }, { task_id: 10 });
             efforts = new Entities.EffortCollection({ task_id: 23 });
+
+            temp_effort_fetch = Entities.Effort.prototype.fetch;
+            Entities.Effort.prototype.fetch = function (options) {
+                options.success(effort, null);
+            };
+
+            temp_efforts_fetch = Entities.EffortCollection.prototype.fetch;
+            Entities.EffortCollection.prototype.fetch = function (options) {
+                options.success(efforts, null);
+            };
+        });
+
+        afterEach(function () {
+            Entities.Effort.prototype.fetch = temp_effort_fetch;
+            Entities.EffortCollection.prototype.fetch = temp_efforts_fetch;
         });
 
         describe('Model', function() {
@@ -78,6 +98,42 @@ define(['app/entities/effort'], function(Entities) {
             it('Should have a url that contains \'effort\'', function() {
                 expect(efforts.url()).toContain('efforts');
                 expect(Entities.EffortCollection.prototype.url()).toContain('efforts');
+            });
+        });
+
+        describe('API', function () {
+            it('Should return a list of efforts', function () {
+                runs(function () {
+                    $.when(App.request('effort:entities', efforts.task_id)).done(function (response) {
+                        expect(response).toBe(efforts);
+                    });
+                });
+            });
+
+            it('Should return a new effort with specified task id', function () {
+                runs(function () {
+                    $.when(App.request('effort:entity', effort.task_id)).done(function (response) {
+                        expect(response.get('id')).toBe(null);
+                        expect(response.task_id).toBe(effort.task_id);
+                    });
+                });
+            });
+
+            it('Should return specified effort', function () {
+                runs(function () {
+                    $.when(App.request('effort:entity', null, effort)).done(function (response) {
+                        expect(response).toBe(effort);
+                    });
+                });
+            });
+
+            it('Should return effort with specified id and task id', function (response) {
+                runs(function () {
+                    $.when(App.request('effort:entity', null, effort.get('id'))).done(function (response) {
+                        expect(response.task_id).toBe(effort.task_id);
+                        expect(response.get('id')).toBe(effort.get('id'));
+                    });
+                });
             });
         });
     });
