7b83f037ea8f621205b824a45b3b03b73b19c426
Sascha Hanssen <sascha.hanssen@ion2s.com>
2014-01-28 14:17:02 +0100

    - renamed jquery to $ in advanced string extend calls

--- client/js/lib/advanced_string.js
+++ client/js/lib/advanced_string.js
@@ -22,7 +22,7 @@ function pad(n, padding, length) {
 
 
 // 'foo_bar' -> 'FooBar'
-jQuery.extend (String.prototype, {
+$.extend(String.prototype, {
     classify: function () {
         return this.replace (/(?:^|[-_])(\w)/g, function (_, c) {
             return c ? c.toUpperCase () : '';
@@ -32,7 +32,7 @@ jQuery.extend (String.prototype, {
 
 
 // 'foo_bar' -> 'fooBar'
-jQuery.extend (String.prototype, {
+$.extend(String.prototype, {
     camelize: function () {
         var s = this.classify();
         return s.charAt(0).toLowerCase() + s.substr(1);
@@ -40,14 +40,14 @@ jQuery.extend (String.prototype, {
 });
 
 // 'FooBar' -> 'foo_bar'
-jQuery.extend (String.prototype, {
+$.extend(String.prototype, {
     underscore: function () {
         return this.replace(/([a-z\d])([A-Z]+)/g, '$1_$2').replace(/[-\s]+/g, '_').toLowerCase();
     }
 });
 
 // ' FooBar ' -> 'FooBar'
-jQuery.extend (String.prototype, {
+$.extend(String.prototype, {
     trim: function () {
         return this.replace(/^\s+|\s+$/g,'');
     }
