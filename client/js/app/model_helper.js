e0d7deb9ebda64fd0b4b949133dce9de1a95fabb
Sascha Hanssen <sascha.hanssen@ion2s.com>
2014-01-29 15:33:52 +0100

    - added doc and test for model helper disabled fields

--- client/js/app/model_helper.js
+++ client/js/app/model_helper.js
@@ -2,6 +2,26 @@
 
 define(function() {
     return {
+        /**
+         * Extracts names of attributes, which should be disabled.
+         *
+         * Checks for keys containing 'IsDefault'. If the according value is false, then the attribute will be
+         * appended to returning result.
+         *
+         * @example
+         * // server response
+         * {
+         *   id: 23,
+         *   name: 'Foo Bar',
+         *   status: 7,
+         *   nameIsDefault: true
+         *   statusIsDefault: false
+         * }
+         * // returns ['status']
+         *
+         * @param attrs {object} - Model attributes given by models parse function.
+         * @returns {string[]} - Array of attributes names, which were set to be disabled.
+         */
         disabled_fields: function(attrs) {
             var token = 'IsDefault';
 
