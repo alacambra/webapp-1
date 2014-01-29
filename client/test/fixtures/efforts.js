c18f99d39cdc3b52f56d116d5e6b302f67343782
Sascha Hanssen <sascha.hanssen@ion2s.com>
2014-01-29 15:07:02 +0100

    - created new fixtures

--- client/test/fixtures/efforts.js
+++ client/test/fixtures/efforts.js
@@ -1,37 +1,37 @@
 define({
     "0": {
         "id": 0,
-        "task_id": 0,
-        "date": 1387206224,
-        "time": 20,
-        "comment": "Comment of Effort 0"
+        "task_id": 2,
+        "date": 1385420400,
+        "time": 195,
+        "comment": "structure"
     },
     "1": {
         "id": 1,
-        "task_id": 0,
-        "date": 1387206224,
-        "time": 25,
-        "comment": "Comment of Effort 1"
+        "task_id": 2,
+        "date": 1385938800,
+        "time": 150,
+        "comment": null
     },
     "2": {
         "id": 2,
-        "task_id": 1,
-        "date": 1387206224,
-        "time": 75,
-        "comment": "Comment of Effort 2"
+        "task_id": 2,
+        "date": 1386975600,
+        "time": 240,
+        "comment": "formatting"
     },
     "3": {
         "id": 3,
-        "task_id": 1,
-        "date": 1387206224,
-        "time": 240,
-        "comment": "Comment of Effort 3"
+        "task_id": 3,
+        "date": 1389654000,
+        "time": 75,
+        "comment": "spellcheck"
     },
     "4": {
         "id": 4,
-        "task_id": 2,
-        "date": 1387206224,
-        "time": 120,
-        "comment": "Comment of Effort 4"
+        "task_id": 5,
+        "date": 1275602400,
+        "time": 210,
+        "comment": null
     }
 });
--- client/test/fixtures/users.js
+++ client/test/fixtures/users.js
@@ -1,34 +1,33 @@
 define({
     "0": {
         "id": 0,
-        "firstName": "Anna",
-        "lastName": "Atlas",
-        "birthDate": 1387206224,
-        "email": "anna@atlas.com"
+        "firstName": "Alice",
+        "lastName": "Riddell",
+        "birthDate": null,
+        "email": "alice.riddell.com"
     },
+
     "1": {
         "id": 1,
         firstName: "Bob",
-        "lastName": "Bubble",
-        "birthDate": 1387206224,
-        "email": "bob@bubble.com"
+        "lastName": "Bishop",
+        "birthDate": -98413200,
+        "email": "b.bishop@mail.com"
     },
+
     "2": {
         "id": 2,
         "firstName": "Charlie",
-        "lastName": "Chaos",
-        "email": "charlie@chaos.com"
+        "lastName": "Brown",
+        "birthDate": null,
+        "email": "charlie@brown.org"
     },
+
     "3": {
         "id": 3,
-        "firstName": "Django",
-        "lastName": "Delta",
-        "email": "django@ion2s.com"
-    },
-    "4": {
-        "id": 4,
-        "firstName": "Erik",
-        "lastName": "Electric",
-        "email": "erik.electric@ions2.com"
+        "firstName": "Dave",
+        "lastName": "Dickinson",
+        "birthDate": 9676800,
+        "email": "dave@ion2s.com"
     }
 });
