75e9ad3040f0c424954a960d72d74898eb07e5dd
Thomas Stern <thomas.stern@ion2s.com>
2014-02-17 11:46:54 +0100

    - changed home view

---
+++ client/app/scripts/controllers/home.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    angular.module('poolingpeopleApp')
+
+        .controller('HomeCtrl', function ($scope) {
+            $scope.title = 'poolingpeople';
+        });
+}());
