b0ebc2a77f379b6c108da55eddb644ee34781a61
Thomas Stern <thomas.stern@ion2s.com>
2014-02-19 17:51:53 +0100

    - added fake server

---
+++ client/app/scripts/fake_server.js
@@ -0,0 +1,37 @@
+(function () {
+	'use strict';
+
+	var dependencies = [
+		'poolingpeopleApp',
+		'ngMockE2E'
+	];
+
+	angular.module('poolingpeopleAppDev', dependencies)
+
+		.run(['$httpBackend', '$log',
+			function ($httpBackend, $log) {
+				var baseUrl = '/webapplication/rest',
+					projects = null,
+					users = null;
+
+				$.get('fixtures/users.json', function (data) {
+					users = data;
+				});
+
+				$.get('fixtures/projects.json', function (data) {
+					projects = data;
+				});
+
+				$httpBackend.whenGET(/.*\.tpl\.html/).passThrough();
+
+//				$httpBackend.whenGET(baseUrl + '/projects').respond(projects, 200, { valid: true });
+
+				$httpBackend.whenGET(baseUrl + '/projects').respond(function (method, url, data, headers) {
+					return [200, JSON.stringify(projects), { valid: true }];
+				});
+
+				$httpBackend.whenGET(baseUrl + '/users').respond(function (method, url, data, headers) {
+					return [200, JSON.stringify(users)];
+				});
+			}]);
+}());
