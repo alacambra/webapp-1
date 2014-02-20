bad3b19a8032ab5ccaf34857c787f346c8f58748
Albert Lacambra <albert.lacambra@ion2s.com>
2014-02-20 17:56:08 +0100

    - tpl for manager

--- server/persistence/neo4j/src/main/java/poolingpeople/persistence/neo4j/inheritance/InheritanceManager.java
+++ server/persistence/neo4j/src/main/java/poolingpeople/persistence/neo4j/inheritance/InheritanceManager.java
@@ -13,44 +13,83 @@ import poolingpeople.persistence.neo4j.entities.*;
 public class InheritanceManager {
 
 	@Inject NeoManager manager;
-	
+
 	public void updateObject(AbstractPersistedModel<?> object){
-		
+
+	}
+
+	public void createEffort(PersistedTask task, PersistedEffort effort) {
+	}           
+	public void createTask(PersistedProject project, PersistedTask parentTask, PersistedTask task){
+	}           
+	public void createUser(PersistedUser user){
+	}           
+	public void createProject(PersistedProject project){
+	}         
+
+	public void updateEffort(PersistedEffort effort) {
+	}   
+	
+	public void updateTask(PersistedTask task){
 	}
 	
+	public void updateUser(PersistedUser user){
+	}
+	
+	public void updateProject(PersistedProject project){
+	}
+
+
+
+	public void deleteEffort(PersistedEffort effort) {
+
+	}
+
+	public void deleteTask(PersistedTask task){
+
+	}
+
+	public void deleteUser(PersistedUser user){
+
+	}
+
+	public void deleteProject(PersistedProject project){
+
+	}
+
 	public void deleteObject(AbstractPersistedModel<?> object){
-		
+
 		Method method[] = object.getClass().getMethods();
-		
+
 		for(int i = 0; i < method.length; i++){
-			
+
 			Method m = method[i];
 			Annotation[] annotations = m.getAnnotations();
-			
+
 			for(int j = 0; j < method.length; j++){
-			
+
 				Annotation a = annotations[j];
 				if ( a.getClass().isAssignableFrom(Slave.class) ){
 					try {
-						
+
 						AbstractPersistedModel<?> slave = (AbstractPersistedModel<?>) m.invoke(object, null);
 						deleteObject(slave);
 						manager.removeNode(object.getNode());
-						
+
 					} catch (IllegalAccessException | IllegalArgumentException
 							| InvocationTargetException e) {
-						
+
 						throw new RootApplicationException(e);
 					}
 				}
 			}
 		}
 	}
-	
+
 	public void createObject(AbstractPersistedModel<?> object){
-		
+
 	}
-	
+
 	private void copyFromTemplate(Object tplObject) {
 
 		Method[] methods = tplObject.getClass().getMethods();
@@ -77,11 +116,11 @@ public class InheritanceManager {
 			} catch (Exception e) {
 				throw new RootApplicationException(
 						"error for method " + tplObject.getClass().getCanonicalName() + "." + dtoMethod.getName() + "|" 
-						+ this.getClass().getCanonicalName() + "." + beanMethod.getName() + ":" + e.getMessage(), e);
+								+ this.getClass().getCanonicalName() + "." + beanMethod.getName() + ":" + e.getMessage(), e);
 			}
 		}
 	}
-	
+
 	private Method getSetterMethod(String getterName, Class<?> param, Object target) {
 
 		String setterName = getterName.replaceAll("^get([A-Z][\\w\\d]+)$", "set$1");
