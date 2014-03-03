34f474e95a42fd0a3ab0b400b919b765278610c2
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-03 18:28:20 +0100

    formatted source file

--- server/webapp-http-server-api/src/main/java/poolingpeople/webapplication/business/boundary/CatchWebExceptionInterceptor.java
+++ server/webapp-http-server-api/src/main/java/poolingpeople/webapplication/business/boundary/CatchWebExceptionInterceptor.java
@@ -17,77 +17,58 @@ import org.codehaus.jackson.map.JsonMappingException;
 import poolingpeople.commons.exceptions.RootApplicationException;
 import poolingpeople.webapplication.business.utils.configuration.boundary.Configurable;
 
-//TODO: test this interceptor via CDI Bean testing concept (in Sprint 1)
 @Interceptor
 @CatchWebAppException
 public class CatchWebExceptionInterceptor {
 
-    @Inject
-    @Configurable("invalidJson")
-    private String invalidJson;
- 
-    @AroundInvoke
-    public Object catchRootException(InvocationContext context) {
-        try {
-        	
-            return context.proceed();
-            
-        } catch (RootApplicationException e) {
-            
-        	throw getSpecificDomainException(e);
-            
-        } catch (JsonGenerationException | JsonParseException generationException) {
-            
-        	throw new WebApplicationException(generationException, Response
-                    .status(Status.BAD_REQUEST).entity(invalidJson).build());
-        	
-        } catch (JsonMappingException jsonMappingException) {
-            
-        	throw new WebApplicationException(jsonMappingException, Response
-                    .status(Status.BAD_REQUEST)
-                    .entity(jsonMappingException.getMessage()).build());
-        	
-        } catch (IOException ioException) {
-            
-        	throw new WebApplicationException(ioException, Response
-                    .status(Status.INTERNAL_SERVER_ERROR)
-                    .entity(ioException.getMessage()).build());
+	@Inject
+	@Configurable("invalidJson")
+	private String invalidJson;
 
-        } catch (WebApplicationException e) {
-        	
-        	throw e;
-        	
-        } catch (Exception e) {
-        	
-        	RootApplicationException rootApplicationException = tryToFindRootApplicationException(e);
-        	
-        	if( rootApplicationException != null ) {
-        		throw getSpecificDomainException(rootApplicationException);
-        	}
-        	
-            throw new WebApplicationException(e, Response
-                    .status(Status.INTERNAL_SERVER_ERROR)
-                    .entity(e.getMessage()).build());
-        }
-    }
-    
-    private WebApplicationException getSpecificDomainException(
-            RootApplicationException exception) {
-        return new WebApplicationException(exception, exception.getSpecificWebResponse());
-    }
-    
-    private RootApplicationException tryToFindRootApplicationException(Throwable e){
+	@AroundInvoke
+	public Object catchRootException(InvocationContext context) {
+		try {
+			return context.proceed();
+		} catch (RootApplicationException e) {
+			throw getSpecificDomainException(e);
+		} catch (JsonGenerationException | JsonParseException generationException) {
+			throw new WebApplicationException(generationException, Response
+					.status(Status.BAD_REQUEST).entity(invalidJson).build());
+		} catch (JsonMappingException jsonMappingException) {
+			throw new WebApplicationException(jsonMappingException, Response
+					.status(Status.BAD_REQUEST)
+					.entity(jsonMappingException.getMessage()).build());
+		} catch (IOException ioException) {
+			throw new WebApplicationException(ioException, Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(ioException.getMessage()).build());
+		} catch (WebApplicationException e) {
+			throw e;
+		} catch (Exception e) {
+			RootApplicationException rootApplicationException = tryToFindRootApplicationException(e);
+			if (rootApplicationException != null) {
+				throw getSpecificDomainException(rootApplicationException);
+			}
+			throw new WebApplicationException(e, Response
+					.status(Status.INTERNAL_SERVER_ERROR)
+					.entity(e.getMessage()).build());
+		}
+	}
 
-    	while(e.getCause() != null) {
-    		
-    		e = e.getCause();
-    		
-    		if(e instanceof RootApplicationException) {
-    			return (RootApplicationException) e;
-    		}
-    	}
-    	
-    	return null;
-    	
-    }
+	private WebApplicationException getSpecificDomainException(
+			RootApplicationException exception) {
+		return new WebApplicationException(exception,
+				exception.getSpecificWebResponse());
+	}
+
+	private RootApplicationException tryToFindRootApplicationException(
+			Throwable e) {
+		while (e.getCause() != null) {
+			e = e.getCause();
+			if (e instanceof RootApplicationException) {
+				return (RootApplicationException) e;
+			}
+		}
+		return null;
+	}
 }
