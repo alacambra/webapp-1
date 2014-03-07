f9b8c0d1c47bbc62b9051152ea0c0fb41832c891
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-07 16:33:58 +0100

    Added Exception handling in CDIInstanceProvider: if given Bean class is not found

--- server/shared/src/main/java/poolingpeople/commons/cdihelper/CDIInstanceProvider.java
+++ server/shared/src/main/java/poolingpeople/commons/cdihelper/CDIInstanceProvider.java
@@ -1,17 +1,21 @@
 package poolingpeople.commons.cdihelper;
 
+import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.spi.CreationalContext;
 import javax.enterprise.inject.spi.Bean;
 import javax.enterprise.inject.spi.BeanManager;
 
 import org.jboss.weld.Weld;
 
+@ApplicationScoped
 public class CDIInstanceProvider implements InstanceProvider{
 
 	@Override
 	public <T> T getInstance(Class<T> clazz) {
 		BeanManager manager = Weld.current().getBeanManager();
 		Bean<?> bean = manager.resolve(manager.getBeans(clazz));
+		
+		if(bean == null) throw new RuntimeException("Bean could not be resolved for Class: " + clazz.toString());
 		
 		CreationalContext<?> cc = manager.createCreationalContext(bean);
 		return clazz.cast(manager.getReference(bean, clazz, cc));
