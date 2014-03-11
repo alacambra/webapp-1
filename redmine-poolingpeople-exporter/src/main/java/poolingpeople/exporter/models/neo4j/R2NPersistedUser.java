75cb3a471ca73b3fb9b9debb3934d60126bdb33b
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-11 13:41:40 +0100

    Removed legacy constructor in User Model

--- redmine-poolingpeople-exporter/src/main/java/poolingpeople/exporter/models/neo4j/R2NPersistedUser.java
+++ redmine-poolingpeople-exporter/src/main/java/poolingpeople/exporter/models/neo4j/R2NPersistedUser.java
@@ -1,15 +1,9 @@
 package poolingpeople.exporter.models.neo4j;
 
-import poolingpeople.commons.entities.User;
-import poolingpeople.persistence.neo4j.NeoManager;
 import poolingpeople.persistence.neo4j.NodePropertyName;
 import poolingpeople.persistence.neo4j.entities.PersistedUser;
 
 public class R2NPersistedUser extends PersistedUser implements IsRedmineEntity{
-
-	public R2NPersistedUser(NeoManager manager, String email, String password, User user) {
-		super(manager, email, password, user);
-	}
 
 	@Override
 	public void setRedmineId(Integer id) {
