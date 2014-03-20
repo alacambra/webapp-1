85950bc971c563dcfa6ba4ce51a569c2f9f6c3d7
Albert Lacambra <albert.lacambra@ion2s.com>
2014-03-20 15:59:44 +0100

    changelog basic api (it returns an empty CL list to avoid eduardo Kartoffelns)

---
+++ server/webapp-http-server-api/src/main/java/poolingpeople/webapplication/business/changelog/boundary/ChagelogMixin.java
@@ -0,0 +1,77 @@
+package poolingpeople.webapplication.business.changelog.boundary;
+
+import java.util.List;
+
+import org.codehaus.jackson.annotate.JsonIgnore;
+import org.codehaus.jackson.annotate.JsonIgnoreProperties;
+import org.codehaus.jackson.map.annotate.JsonView;
+import org.neo4j.graphdb.Node;
+
+import poolingpeople.commons.entities.ChangeLog;
+import poolingpeople.commons.entities.ChangeLogAction;
+import poolingpeople.commons.entities.Comment;
+import poolingpeople.commons.entities.PoolingpeopleEntity;
+import poolingpeople.commons.entities.Subject;
+import poolingpeople.commons.entities.User;
+import poolingpeople.webapplication.business.boundary.JsonViews;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ChagelogMixin implements ChangeLog{
+
+	@Override
+	@JsonView(JsonViews.Shared.class)
+	public String getId() {
+		return null;
+	}
+	
+
+	@JsonIgnore
+	public Node getNode() {
+		return null;
+	}
+
+
+	@Override
+	public Subject getSubject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public ChangeLogAction getAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Long getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void setSubject(Subject subject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setAction(ChangeLogAction action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setDate(Long date) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+}
---
+++ server/webapp-http-server-api/src/main/java/poolingpeople/webapplication/business/changelog/boundary/ChangelogDTO.java
@@ -0,0 +1,101 @@
+package poolingpeople.webapplication.business.changelog.boundary;
+
+import java.util.List;
+
+import poolingpeople.commons.entities.ChangeLog;
+import poolingpeople.commons.entities.ChangeLogAction;
+import poolingpeople.commons.entities.Comment;
+import poolingpeople.commons.entities.PoolingpeopleEntity;
+import poolingpeople.commons.entities.Subject;
+import poolingpeople.commons.entities.User;
+
+public class ChangelogDTO implements ChangeLog {
+
+	private String comment;
+	private Long date;
+
+
+	@Override
+	public Long getDate() {
+		return date;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	public void setDate(Long date) {
+		this.date = date;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Subject getSubject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ChangeLogAction getAction() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setSubject(Subject subject) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setAction(ChangeLogAction action) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
