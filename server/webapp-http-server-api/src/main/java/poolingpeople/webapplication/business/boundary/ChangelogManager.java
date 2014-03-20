5ae70791d16f8983d990b9b0a1ceb5f5c7f9174c
Qaiser Abbasi <mta.umer@gmail.com>
2014-03-20 15:01:34 +0100

    implemented dummy ChangeLog manager (event based)

--- server/webapp-http-server-api/src/main/java/poolingpeople/webapplication/business/boundary/ChangelogManager.java
+++ server/webapp-http-server-api/src/main/java/poolingpeople/webapplication/business/boundary/ChangelogManager.java
@@ -1,6 +1,8 @@
-package poolingpeople.persistence.neo4j.entities;
+package poolingpeople.webapplication.business.boundary;
 
 import javax.enterprise.event.Observes;
+
+import poolingpeople.persistence.neo4j.entities.PersistedTask;
 
 public class ChangelogManager {
 
